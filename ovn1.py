3326a051a573f56d990f59ec590d4e9300291093
Daniel Bosk <dbosk@kth.se>
2023-09-04 11:34:23 +0200

    Lade till subtrahera, fler exempel med f-strängar

--- ovn1.py
+++ ovn1.py
@@ -1,8 +1,16 @@
 """Olles repeterad"""
 
-def addera(a, b):
-    """Adderar talen a och b och returnerar summan"""
-    summan = a + b
-    return summan
+def addera(term1, term2):
+    """Adderar termerna a och b och returnerar summan"""
+    return term1 + term2 
 
-print(addera(5, 7))
+def subtrahera(term1, term2):
+    """Subtraherar termerna a och b och returnerar differensen"""
+    return term1 - term2
+
+print("5 + 7 = {addera(5, 7)} (utan f)")
+print(f"5 + 7 = (addera(5, 7)) (parenteser)")
+print(f"5 + 7 = {addera(5, 7)} (korrekt)")
+print(f"5 + 7 = {5+7} (5+7 direkt)")
+print("5 + 7 = " + str(addera(5, 7)) + " (str)")
+print(f"5 - 7 = {subtrahera(5, 7)}")
